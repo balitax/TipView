9087c6bf4b309f862824cbeb0566d4790c6b6720
Abay <a.kenzhekhan@aviata.me>
2021-01-04 00:19:08 +0600

    initial commit

---
+++ Contents.swift
@@ -0,0 +1,53 @@
+import UIKit
+import PlaygroundSupport
+
+class TipViewController: UIViewController {
+    
+    private let messageLabel: UILabel = {
+        let messageLabel = UILabel()
+        messageLabel.text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged"
+        messageLabel.textColor = .white
+        messageLabel.numberOfLines = 0
+        return messageLabel
+    }()
+    
+    private lazy var showButton: UIButton = {
+        let button = UIButton(type: .system, primaryAction: UIAction(handler: { _ in
+            self.showTipView()
+        }))
+        button.layer.borderColor = UIColor.link.cgColor
+        button.layer.borderWidth = 1.0
+        button.contentEdgeInsets = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+        button.layer.cornerRadius = 8
+        button.setTitle("Show tip", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(showButton)
+        NSLayoutConstraint.activate([
+            showButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            showButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+    
+    private func showTipView() {
+        let tipPreferences = TipView.TipViewPrefenreces(
+            messageView: messageLabel,
+            sourceView: showButton,
+            containerView: view,
+            position: .upon,
+            leftMargin: 16,
+            rightMargin: 16
+        )
+        let tipView = TipView(preferences: tipPreferences)
+        tipView.show()
+    }
+}
+
+
+let vc = TipViewController()
+
+PlaygroundPage.current.liveView = vc
---
+++ Sources/TipView.swift
@@ -0,0 +1,114 @@
+import Foundation
+import UIKit
+
+import UIKit
+
+public class TipView: UIView {
+    
+    public struct TipViewPrefenreces {
+        let messageView: UIView
+        let sourceView: UIView
+        weak var containerView: UIView?
+        let position: TipPosition
+        let leftMargin: CGFloat
+        let rightMargin: CGFloat
+        
+        public init(messageView: UIView, sourceView: UIView, containerView: UIView? = nil, position: TipView.TipPosition, leftMargin: CGFloat, rightMargin: CGFloat) {
+            self.messageView = messageView
+            self.sourceView = sourceView
+            self.containerView = containerView
+            self.position = position
+            self.leftMargin = leftMargin
+            self.rightMargin = rightMargin
+        }
+    }
+    
+    public enum TipPosition {
+        case upon
+        case below
+    }
+    
+    public let preferences: TipViewPrefenreces
+    private let tailWidth: CGFloat = 20
+    private let tailHeight: CGFloat = 10
+    
+    private lazy var tailView: UIView = {
+        let tailView = UIView()
+        tailView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(tailView)
+        NSLayoutConstraint.activate([
+            tailView.widthAnchor.constraint(equalToConstant: tailWidth),
+            tailView.heightAnchor.constraint(equalToConstant: tailWidth),
+            preferences.position == .below ? tailView.bottomAnchor.constraint(equalTo: topAnchor) : tailView.topAnchor.constraint(equalTo: bottomAnchor),
+            tailView.centerXAnchor.constraint(equalTo: preferences.sourceView.centerXAnchor)
+        ])
+        return tailView
+    }()
+    
+    public init(preferences: TipViewPrefenreces) {
+        self.preferences = preferences
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func show() {
+        setup()
+        showTail()
+    }
+    
+    private func showTail() {
+        if preferences.position == .below {
+            tailView.transform = tailView.transform.rotated(by: CGFloat(Double.pi))
+        }
+        let tailPath = getTailPath()
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.fillColor = UIColor.black.cgColor
+        shapeLayer.path = tailPath
+        tailView.layer.addSublayer(shapeLayer)
+    }
+    
+    private func getTailPath() -> CGPath {
+        let bezier = UIBezierPath()
+        bezier.move(to: CGPoint(x: tailWidth / 2, y: tailHeight))
+        bezier.addCurve(to: CGPoint(x: 0, y: 0), controlPoint1: CGPoint(x: tailWidth / 2, y: tailHeight / 2), controlPoint2: CGPoint(x: 0, y: 0))
+        bezier.addLine(to: CGPoint(x: tailWidth, y: 0))
+        bezier.addCurve(to: CGPoint(x: tailWidth / 2, y: tailHeight), controlPoint1: CGPoint(x: tailWidth, y: 0), controlPoint2: CGPoint(x: tailWidth / 2, y: tailHeight / 2))
+        bezier.close()
+        return bezier.cgPath
+    }
+    
+    @objc func dismiss() {
+        removeFromSuperview()
+    }
+    
+    private func setup() {
+        configureLook()
+        preferences.containerView?.addSubview(self)
+        addSubview(preferences.messageView)
+        setBaseConstraints()
+    }
+    
+    private func setBaseConstraints() {
+        [self, preferences.messageView, preferences.sourceView].forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
+        guard let superview = superview else { return }
+        NSLayoutConstraint.activate([
+            leadingAnchor.constraint(equalTo: superview.leadingAnchor, constant: preferences.leftMargin),
+            trailingAnchor.constraint(equalTo: superview.trailingAnchor, constant: -preferences.rightMargin),
+            preferences.position == .upon ? bottomAnchor.constraint(equalTo: preferences.sourceView.topAnchor, constant: -tailHeight) : topAnchor.constraint(equalTo: preferences.sourceView.bottomAnchor, constant: tailHeight),
+            preferences.messageView.topAnchor.constraint(equalTo: topAnchor, constant: 8),
+            preferences.messageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8),
+            preferences.messageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
+            preferences.messageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8)
+        ])
+    }
+    
+    private func configureLook() {
+        backgroundColor = UIColor.black
+        layer.cornerRadius = 8
+        layer.opacity = 0.95
+        preferences.messageView.backgroundColor = .clear
+    }
+}
